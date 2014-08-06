1806aed8b8de1cffb4d07693185ac85e06c12386
Vincent Grafe <vincent.grafe@gmail.com>
2014-08-06 17:42:11 -0400

    e2e tests for login

--- test/e2e/login.scenario.js
+++ test/e2e/login.scenario.js
@@ -1,26 +1,33 @@
 /**
  * Created by vgrafe on 8/6/14.
  */
-describe('angularjs login', function() {
+describe('scanprintMobile login scenarios', function () {
 
     var ptor;
 
-    beforeEach(function() {
+    beforeEach(function () {
         browser.get('http://m.scanprint.net/');
         ptor = protractor.getInstance();
     });
 
-    it('should not log random user', function() {
+    it('should not log random user', function () {
         expect(browser.getTitle()).toEqual('http://m.scanprint.net/');
     });
 
-    it('should not display error message', function() {
-        expect(ptor.isElementPresent(by.id('repoform'))).toBe(false);
+    it('should not display an error message', function () {
+        expect(ptor.isElementPresent(by.id('errorMessage'))).toBe(false);
     });
 
-    it('should not log random user', function() {
-        element(by.model('vm.credentials.login')).sendKeys('oisajoaij sdioj');
-        element(by.model('vm.credentials.password')).sendKeys('asdasd');
-        element(by.id('loginButton')).click();
+    describe('when loggin a wrong user', function () {
+
+        beforeEach(function () {
+            element(by.model('vm.credentials.login')).sendKeys('oisajoaij sdioj');
+            element(by.model('vm.credentials.password')).sendKeys('asdasd');
+            element(by.id('loginButton')).click();
+        });
+
+        it('should display an error message', function () {
+            expect(ptor.isElementPresent(by.id('errorMessage'))).toBe(true);
+        });
     });
 });
